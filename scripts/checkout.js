132e38d75e05a95ff6f36df235c05f6debd00f6a
Ahmed Thomas <71823119+AhmedThomas@users.noreply.github.com>
2025-08-22 18:57:57 +0300

    convert products into classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,5 @@
 import { renderOrderSummary } from './checkout/order-summary.js';
 import { renderPaymentSummary } from './checkout/payment-summary.js';
-import '../data/oop-cart.js';
-import '../data/class-cart.js';
 
 renderOrderSummary();
 renderPaymentSummary();
