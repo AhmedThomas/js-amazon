dec9ee6baa37168abf625456808823088373c31a
Ahmed Thomas <71823119+AhmedThomas@users.noreply.github.com>
2025-08-22 18:32:58 +0300

    created class version of the cart

--- data/oop-cart.js
+++ data/oop-cart.js
@@ -86,5 +86,5 @@ businessCart.loadFromStorage();
 
 businessCart.addToCart('83d4ca15-0f35-48f5-b7a3-1ea210004f2e');
 
-console.log(cart);
-console.log(businessCart);
+// console.log(cart);
+// console.log(businessCart);
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from './checkout/order-summary.js';
 import { renderPaymentSummary } from './checkout/payment-summary.js';
 import '../data/oop-cart.js';
+import '../data/class-cart.js';
 
 renderOrderSummary();
 renderPaymentSummary();
